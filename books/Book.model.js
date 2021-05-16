66f8e721fa676faf472f04369f529314fd6bb614
ffrancon <ffranconwd@gmail.com>
2021-05-16 18:24:58 +0200

    owner id required

--- books/Book.model.js
+++ books/Book.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const BookSchema = new mongoose.Schema({
   owner: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'user'
+    ref: 'user',
+    required: true
   },
   name: {
     type: String,
